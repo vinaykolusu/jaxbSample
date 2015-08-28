d61d5685b8cd4fac2d28e25f97a3b967fdc78080
vinaykolusu <vinaykolusu@users.noreply.github.com>
2015-08-28 15:51:16 -0400

    Create EmployeeContactHome.java

---
+++ EmployeeContactHome.java
@@ -0,0 +1,28 @@
+package com.vinaykolusu.projects.jaxbsample;
+
+public class EmployeeContactHome {
+	
+	private String eContactNumberHome;
+
+	public String geteContactNumberHome() {
+		return eContactNumberHome;
+	}
+
+	public void seteContactNumberHome(String eContactNumberHome) {
+		this.eContactNumberHome = eContactNumberHome;
+	}
+
+	public EmployeeContactHome(String eContactNumberHome) {
+		super();
+		this.eContactNumberHome = eContactNumberHome;
+	}
+
+	public EmployeeContactHome() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	
+	
+	
+}
