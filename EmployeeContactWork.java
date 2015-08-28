49b137f813380cb8c2661141f834d8de7120f0b6
vinaykolusu <vinaykolusu@users.noreply.github.com>
2015-08-28 15:51:44 -0400

    Create EmployeeContactWork.java

---
+++ EmployeeContactWork.java
@@ -0,0 +1,26 @@
+package com.vinaykolusu.projects.jaxbsample;
+
+public class EmployeeContactWork {
+	
+	private String eContactNumberWork;
+
+	public EmployeeContactWork() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public EmployeeContactWork(String eContactNumberWork) {
+		super();
+		this.eContactNumberWork = eContactNumberWork;
+	}
+
+	public String geteContactNumberWork() {
+		return eContactNumberWork;
+	}
+
+	public void seteContactNumberWork(String eContactNumberWork) {
+		this.eContactNumberWork = eContactNumberWork;
+	}
+	
+
+}
